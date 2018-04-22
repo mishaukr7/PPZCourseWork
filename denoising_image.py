9895e4ec2e3534ed9c0da551b2962dd49f5ebe82
misha <mishaukr22@gmail.com>
2018-04-22 23:35:01 +0300

    end.

--- denoising_image.py
+++ denoising_image.py
@@ -13,10 +13,10 @@ def denoising_image(image, wavelet, level_of_transformation, threshold, mode_thr
     :return: denoised image array;
     """
 
-    image_array = image_extract_array.get_image_array_with_noise(image, 10)
-    Y = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.299, 0.578, 0.114)
-    U = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.1678, -0.3313, 0.5)
-    V = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.5, -0.4187, 0.0813)
+    image_array = image_extract_array.get_image_array(image)
+    Y = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.299, 0.587, 0.114)
+    #U = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.1678, -0.3313, 0.5)
+    #V = image_extract_array.transform_coefficients_from_rgb_to_yuv(image_array, 0.5, -0.4187, 0.0813)
 
     WaveletCoefficients = wavelet_thresholding.wavelet_decomposition(Y, wavelet, level_of_transformation)
     thresholded_wavelet = wavelet_thresholding.wavelet_thresholding(WaveletCoefficients, threshold, mode_thresholding)
@@ -25,11 +25,12 @@ def denoising_image(image, wavelet, level_of_transformation, threshold, mode_thr
 
     return denoised_array
 #
+
 # print("FIRST:")
-# print(image_extract_array.get_image_array_with_noise('temp.jpg', 10)[0][:20])
+#print(image_extract_array.get_image_array_with_noise('temp.jpg', 10)[0][:20])
 # print("RESULT:")
 # print(denoising_image('temp.jpg', 'db1', 1, 5, 'soft')[0][:20])
-#main.show_image(denoising_image('temp.jpg', 'db2', 1, 10, 'soft'))
+#main.show_image(denoising_image('images/temp.jpg', 'db1', 1, 0, 'soft'))
 #print(denoising_image('a713.jpg', 'db1', 2, [5, 5, 5, 5], 'soft'))
 #main.image_show_PIL(denoising_image('temp.jpg', 'db1', 2, [5, 5, 5, 5], 'soft'))
 
